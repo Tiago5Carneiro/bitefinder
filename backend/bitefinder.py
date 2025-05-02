13bac600ecda7bccca8fc675964fbb79298136ef
guedes674 <tiago.matos.guedes555@gmail.com>
2025-05-02 02:28:54 +0100

    schema and conn initiated

---
+++ backend/bitefinder.py
@@ -0,0 +1,13 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = mysql.connector.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_DATABASE"),
+    port=int(os.getenv("DB_PORT"))
+)
