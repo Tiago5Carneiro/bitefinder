233ac7a54f3ad8a55a7b16ac592261676ea53b0e
Diogoacg <a101919@alunos.uminho.pt>
2025-05-02 03:22:58 +0100

    auth

--- backend/schema.sql
+++ backend/schema.sql
@@ -5,38 +5,37 @@ CREATE TABLE IF NOT EXISTS user (
     username VARCHAR(100) PRIMARY KEY,
     name VARCHAR(80) NOT NULL,
     password VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS restaurant {
-    id INT AUTO_INCREMENT PRIMARY_KEY,
+CREATE TABLE IF NOT EXISTS restaurant (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     rating FLOAT NOT NULL,
-    url_location VARCHAR(255) NOT NULL,
+    url_location VARCHAR(255) NOT NULL
+);
 
-}
-
-CREATE TABLE IF NOT EXISTS group {
+CREATE TABLE IF NOT EXISTS `group` (
     code VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     status ENUM('active', 'inactive') NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-}
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE IF NOT EXISTS group_user (
     group_code VARCHAR(10),
     username VARCHAR(100),
-    PRIMARY KEY (group_code, username),
+    PRIMARY KEY (group_code, username)
 );
 
 CREATE TABLE IF NOT EXISTS user_restaurant (
     username VARCHAR(100),
     restaurant_id INT,
-    PRIMARY KEY (username, restaurant_id),
+    PRIMARY KEY (username, restaurant_id)
 );
 
 CREATE TABLE IF NOT EXISTS restaurant_image (
     restaurant_id INT,
     image_url VARCHAR(255),
-    PRIMARY KEY (restaurant_id, image_url),
+    PRIMARY KEY (restaurant_id, image_url)
 );
