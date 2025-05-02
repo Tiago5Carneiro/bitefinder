fd5b12b1e4bb5381afa245354d79d33e02de6b36
GoncaloCoutoDosSantos <a93279@alunos.uminho.pt>
2025-05-02 14:05:05 +0100

    add vector data

--- src/vectorization/vectorization.py
+++ src/vectorization/vectorization.py
@@ -1,4 +1,6 @@
 import os
+import json
+from time import sleep
 from PIL import Image
 from dotenv import load_dotenv
 
@@ -35,7 +37,7 @@ def creating_embeddings_from_text(input):
 
     print(response.data[0].embedding)
 
-    return response
+    return response.data[0].embedding
 
 def starting_gemini_client():
     from google import genai
@@ -55,6 +57,20 @@ def path_to_image(path):
     image = Image.open(path)
     return image
 
+def gemini_generate_text_from_image(image,prompt):
+    response = None
+
+    while response == None:
+        try: 
+            response = gemini_client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=[image, prompt]
+            )
+        except:
+            print(f"sleeping {prompt}")
+            sleep(61)
+    return response
+
 def detect_image_type(image):
     prompt ="""
     You are a helpful assistant that can analyze images.
@@ -65,10 +81,8 @@ def detect_image_type(image):
     
     Do not include any explanation, bounding boxes, JSON, or other text in your response.
     """
-    response = gemini_client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=[image, prompt]
-    )
+    response = gemini_generate_text_from_image(image,prompt)
+
     return response.text
 
 def text_from_restaurant_image(image):
@@ -76,22 +90,18 @@ def text_from_restaurant_image(image):
     Here is an image of a restaurant, I want you to give me a brief description (between 50 to 100) of this restaurants style,
     colour theme, mood and vibe.
     """
-    response = gemini_client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=[image, prompt]
-    )
-    print(response.text)
+    response = gemini_generate_text_from_image(image,prompt)
+
+    return response.text
 
 def text_from_food_image(image):
     prompt ="""
     Here is an image of a menu item, I want you to give me a brief description (between 50 to 100) of this food's style,
     , mood, colourfullness and vibe.
     """
-    response = gemini_client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents=[image, prompt]
-    )
-    print(response.text)
+    response = gemini_generate_text_from_image(image,prompt)
+
+    return response.text
 
 def create_image_text(path, url = 0):
     if url == 1:
@@ -100,16 +110,58 @@ def create_image_text(path, url = 0):
         image = path_to_image(path)
     food_type = int(detect_image_type(image))
     print(food_type)
+    ret = (None,None)
     if food_type == 0:
-        text_from_restaurant_image(image)
-    elif food_type ==1:
-        text_from_food_image(image)
+        ret = (0,text_from_restaurant_image(image))
+    elif food_type == 1:
+        ret = (1,text_from_food_image(image))
+        
+    return ret
+
+def create_embeddings_file(path):
+    starting_gemini_client()
+    starting_mistral_client()
+
+    f = open(path,"r")
+    data = json.load(f)
+
+    ret_dic = {}
+
+    for ind_p in data:
+        if data[ind_p]["primaryType"] in ["shopping_mall","hotel","cultural_center"]:
+            pass
+
+        place = data[ind_p]
+        food_v = None
+        restaurant_v = None
+
+        for image_ind in range(min(len(place["photos"]),4)):
+            if(food_v and restaurant_v):
+                pass
+
+            image_url = place["photos"][image_ind]
+            image_type, text = create_image_text(image_url,1)
+
+            if image_type == 1 and food_v == None:
+                food_v = text_from_food_image(text)
+                food_v = creating_embeddings_from_text(food_v)
+            elif image_type == 0 and restaurant_v == None:
+                restaurant_v = text_from_restaurant_image(text)
+                restaurant_v = creating_embeddings_from_text(restaurant_v)
+
+        ret_dic[ind_p] = {"foodVector": food_v, "restaurantVector": restaurant_v}
+
+    f.close()
+    f = open(path + "_vec","w")
+    f.write(json.dumps(ret_dic))
+    f.close()
 
 
-starting_gemini_client()
 
-create_image_text(text_restauraunt_url,1)
-create_image_text(test_url,1)
+#starting_gemini_client()
+
+#create_image_text(text_restauraunt_url,1)
+#create_image_text(test_url,1)
 
 
 #starting_mistral_client()
